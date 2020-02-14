3febfeefbc8e8e15a446b7aa810dfede6d7a6a12
Simeon <drsimeon_87@abv.bg>
2020-02-14 07:06:09 +0200

    secWs

---
+++ ModulesAndRouting/causes/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { RightComponent } from './right/right.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { LeftComponent } from './left/left.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RightComponent,
+    LeftComponent,
+    FooterComponent,
+    HomeComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    CoreModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
