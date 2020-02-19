cf106287a8a012d16da7dfa261d2dcfd755741fa
Simeon <drsimeon_87@abv.bg>
2020-02-19 09:11:28 +0200

    1902Start

---
+++ PipesInterceptorsAndSubject/directivesAndForms/src/app/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  email = 'test@abv.bg';
+  password= 'test';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  loginHandler(data) {
+    console.log(data);
+    
+  }
+
+}
